47e6444186c4d8be7be5309cfe5fdf790d6b8996
João <joaovictor.ramos34@gmail.com>
2023-03-09 09:10:16 -0300

    Refatorando o código

--- Aula 2/exercicios/ex02.py
+++ Aula 2/exercicios/ex02.py
@@ -1,3 +1,2 @@
 salario = float(input('Digite o valor de seu sálario: '))
-comissao = (50/100)
-print(f'O valor acrescido com comissão de 5% é: {(comissao*salario)+salario:.0f} reais')
+print(f'O valor acrescido com comissão de 5% é: {((5/100)*salario)+salario:.0f} reais')

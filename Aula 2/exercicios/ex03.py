0efd91a4bfa3f74714ada5b00029521b9878b8d1
João <joaovictor.ramos34@gmail.com>
2023-03-09 09:30:10 -0300

    Resolução do exercicio 3

---
+++ Aula 2/exercicios/ex03.py
@@ -0,0 +1,3 @@
+distancia = float(input('Digite a distância entre duas cidades: '))
+tempo = float(input('Digite o tempo gasto entre duas cidades: '))
+velocidadeMedia = print(f'A velocidade média é de: {(distancia/tempo):.0f} km/h')

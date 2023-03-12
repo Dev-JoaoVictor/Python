e1dccf1f3ebe7c7dd3d0b5ec7dbfa98a624c07b4
João <joaovictor.ramos34@gmail.com>
2023-03-12 14:20:07 -0300

    Resolução do exercicio

--- Aula 2/exercicios/ex07.py
+++ Aula 2/exercicios/ex07.py
@@ -1,5 +1,5 @@
 print('Conversor de temperatura')
 celsius = float(input('Informe a temperatura: '))
-# tf = 1,8 * celsius + 32
+
 print(f'O valor convertido para Fahrenheit é: {(1.8 * celsius) + 32}')
 print(f'O valor convertido para Kelvin é: {celsius + 273 }')
